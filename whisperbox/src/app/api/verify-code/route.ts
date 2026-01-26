0c492e91256be3af92b4d3b4ecaea77d09109c18
himanshu7437 <himanshujangra368@gmail.com>
2026-01-26 14:59:13 +0530

    Code Push + Api Routes Testing - verify code api route added and successfull test all api routes on postman

--- whisperbox/src/app/api/verify-code/route.ts
+++ whisperbox/src/app/api/verify-code/route.ts
@@ -1,5 +1,51 @@
 import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/model/User.model";
-import { success, z } from "zod";
-import { usernameValidation } from "@/schemas/signUpSchema";
 
+export async function POST(request: Request) {
+    await dbConnect();
+
+    try {
+        const {username, code} = await request.json();
+
+        const decodeUsername = decodeURIComponent(username);
+        const user = await UserModel.findOne({username: decodeUsername});
+
+        if(!user) {
+            return Response.json({
+            success: false,
+            message: "User not found"
+            }, { status: 500 })
+        }
+
+        const isCodeValid = user.verifyCode === code;
+        const isCodeNotExpired = new Date(user.verifyCodeExpiry) > new Date();
+
+        if (isCodeValid && isCodeNotExpired) {
+            user.isVerified = true;
+            await user.save();
+            
+            return Response.json({
+            success: true,
+            message: "Account Verified Successfully"
+            }, { status: 200 })
+        } else if(!isCodeNotExpired) {
+            return Response.json({
+            success: false,
+            message: "verification code has expired please signup again to get new code"
+            }, { status: 500 })
+        } else {
+            return Response.json({
+            success: false,
+            message: "Incorrect Verificaion Code"
+            }, { status: 500 })
+        }
+
+
+    } catch (error) {
+        console.log("Error verifying user", error);
+        return Response.json({
+            success: false,
+            message: "Error verifying user"
+        }, { status: 500 })
+    }
+}
