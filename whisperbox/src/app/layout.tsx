e13a1069eab87c200099024a58611dc0f3c4b39a
himanshu7437 <himanshujangra368@gmail.com>
2026-01-30 14:20:50 +0530

    frontend + testing - signup page - successfully added signup page with manual testing. || bug - signin name instead of signup, status code in check username (400 -> 200), authprovider placement in layout.tsx || dependency - input component from shadcn

--- whisperbox/src/app/layout.tsx
+++ whisperbox/src/app/layout.tsx
@@ -26,14 +26,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <AuthProvider>
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
+          >
+          <AuthProvider>
           {children}
           <Toaster />
+          </AuthProvider>
         </body>
-      </AuthProvider>
     </html>
   );
 }
